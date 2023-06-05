80b1ad2e3d2dca8bdf1feefe1f0383b6546cf06f
jonjigoncalves <jonjigoncalves@gmail.com>
2023-06-05 10:10:31 -0400

    done with readme

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', ']', '{', '}', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/'];
 
 var combined = lowercase.concat(uppercase).concat(numbers).concat(specialCharacters);
-// function for length
+// to geth the userinput for lenght of the password and the characters they want to include
 function getUserInput (){  
   var amount = Number(prompt('Please Provide an Amount'));
   var includeLowercase = confirm('Click OK to include lowercase letters');
@@ -18,7 +18,7 @@ function getUserInput (){
 
   return [amount, includeLowercase, includeUppercase, includeSpecial, includeNumbers];
 }
-
+// next step is to create a function so that pass word can be added to the string below based on the users choices.
 function generatePassword(){
   var password = '';
   var choices = getUserInput();
@@ -28,7 +28,7 @@ function generatePassword(){
   var includeNumbers = choices [4];
   var charAmount = choices [0];
   var allpossiblechars = [];
-
+// create conditional statments to get the possible characters that are availble to add to the string
   if (includeLowercase) {
     allpossiblechars = allpossiblechars.concat(lowercase);
     // console.log (allpossiblechars);
@@ -46,7 +46,7 @@ function generatePassword(){
     allpossiblechars = allpossiblechars.concat(numbers);
     
   }
-// for loops consists of three parts  the inital (where you name a varaiable, then the CSSConditionRule, then the increment)
+//this for loop consists of three parts  the inital (where you name a varaiable, then the ConditionRule, then the increment)
 for ( var count = 0; count < charAmount; count++) {
 //next step is to grab a random character from the allpossible chars array 
 var randomNum = Math.random();
@@ -56,6 +56,7 @@ var randomChar = allpossiblechars [randomIndex];
 
 password += randomChar;
 }
+// since this function is working to gives a value, we need a return value so that we dont get a undefined  back 
 return password; 
 
 }
@@ -66,8 +67,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   // create a var for the legnth of the combined array, because the charlist is located inside the generatePassword function and cant be used outside of it unless defined in the global area.
-  var length =combined.length;
-  var password = generatePassword();
+   var password = generatePassword();
   
 
   var passwordText = document.querySelector("#password");  
